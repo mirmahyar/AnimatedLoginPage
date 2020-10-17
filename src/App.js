6817ab489575481a87e9db79eda9fdd3e0aae32c
mirmahyar <m.mirsamadi@gmail.com>
2020-10-18 01:37:25 +0330

    many enhancements, added animation using react-spring framework

--- src/App.js
+++ src/App.js
@@ -1,41 +1,78 @@
+import react from 'react';
 import React, { useState } from 'react';
-import { useSpring, animated } from "react-spring"
+import { useSpring, animated } from 'react-spring';
 
 import './App.css';
 
 function App() {
 
+  const [registrationFormStatus, setRegistrationFormStatus] = useState(false)
+  const loginProps = useSpring({
+    left: registrationFormStatus ? -430 : 0,
+    opacity: registrationFormStatus ? 0 : 1
+  })
+  const registerProps = useSpring({
+    left: registrationFormStatus ? 0 : 430,
+    opacity: registrationFormStatus ? 1 : 0
+  })
+  const loginBtnProps = useSpring({
+    borderBottom: registrationFormStatus ? "solid 0px transparent" : "solid 2px #1059FF",
+    fontWeight: registrationFormStatus ? "100" : "700",
+    fontSize: registrationFormStatus ? "medium" : "x-large"
+  })
+  const registerBtnProps = useSpring({
+    borderBottom: registrationFormStatus ? "solid 2px #1059FF" : "solid 0px transparent",
+    fontSize: registrationFormStatus ? "x-large" : "medium"
+  })
+
+  const registerClicked = () => {
+
+    setRegistrationFormStatus(true)
+  }
+
+  const loginClicked = () => {
+
+    setRegistrationFormStatus(false)
+  }
+
+
+
   return (
     <div className="login-register-wrapper">
       <div className="Nav-buttons">
-        <button id="loginBtn" className='active'>Log in</button>
-        <button id="registerBtn" >Register</button>
+        <animated.button onClick={loginClicked} id="loginBtn" style={loginBtnProps}>Log in</animated.button>
+        <animated.button onClick={registerClicked} id="registerBtn" style={registerBtnProps}>Sign Up</animated.button>
       </div>
+
+
       <div className="form-group">
-        <LoginForm />
-        <RegisterForm />
+        <animated.form action='' id='loginform' style={loginProps}><LoginForm /></animated.form>
+        <animated.form action='' id='registerform' style={registerProps}><RegisterForm /></animated.form>
       </div>
-      <div className="forgot-panel"> forgot passowrd?</div>
+
+      <div className="forgot-panel">
+        <a href='#'> forgot passoword?</a>
+      </div>
 
     </div>
-  )
+  );
 }
 
 const LoginForm = () => {
   return (
-    <form action='' id='loginform'>
+    <react.Fragment>
       <label for='username'>USERNAME</label>
       <input type='text' id='username' />
       <label for='password'>PASSWORD</label>
       <input type='text' id='password' />
       <input type="submit" value='submit' className='submit' />
-    </form>
+    </react.Fragment>
   )
 }
 
 const RegisterForm = () => {
   return (
-    <form action='' id='registerform'>
+    <react.Fragment>
       <label for='fullname'>Full Name</label>
       <input type="text" id="fullname" />
       <label for='email'>Email</label>
@@ -45,7 +82,7 @@ const RegisterForm = () => {
       <label for='confirmpassword'>Confirm passowrd</label>
       <input type="text" id="confirmpassword" />
       <input type="submit" value="submit" className='submit' />
-    </form>
+    </react.Fragment>
   )
 }
 
